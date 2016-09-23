291ab7ad941a888ee1504b943e77cef4b8d6f957
Vidya Ramakrishnan <vidya@hasgeek.in>
2016-09-23 18:50:38 +0530

    Add clipboard.js to assets.

--- baseframe/assets.py
+++ baseframe/assets.py
@@ -194,6 +194,8 @@ assets['moment.js'][Version('2.14.1')] = 'baseframe/js/moment.js'
 assets['bootstrap-daterangepicker.js'][Version('2.1.24')] = ('jquery.js>=1.8.3', 'bootstrap.js>=3.3.1', 'moment.js', 'baseframe/js/bootstrap-daterangepicker.js')
 assets['bootstrap-daterangepicker.css'][Version('2.1.24')] = ('bootstrap.css>=3.3.1', 'baseframe/css/bootstrap-daterangepicker.css')
 
+assets['clipboard.js'][Version('1.5.12')] = 'baseframe/js/clipboard.js'
+
 # Font Awesome
 assets['fontawesome.css'][Version('3.1.0')] = 'baseframe/css/fontawesome-3.1.css'
 assets['fontawesome.css'][Version('4.4.0')] = 'baseframe/css/fontawesome-4.4.css'
